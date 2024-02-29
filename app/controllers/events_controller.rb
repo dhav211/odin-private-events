ae4ca0c1b1b6faa55a29c8bc76797843d9c0a316
dhav211 <dhav211@gmail.com>
2024-02-29 09:32:53 -0800

    Add form to create new event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,28 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def show
+  end
+
+  def new
+    @new_event = Event.new
+  end
+
+  def create
+    @new_event = Event.new allowed_params
+    @new_event.creator = current_user
+
+    if @new_event.save
+      redirect_to root_path, notice: 'Event created sucessfully!'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def allowed_params
+    params.require(:event).permit(:name, :location, :date, :creator)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :event, only: [:index]
+  resources :events, only: [:index, :new, :create]
   resources :users, only: [:show]
 
   # Defines the root path route ("/")
